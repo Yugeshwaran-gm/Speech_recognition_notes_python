899865756d36386337203a733894351357f6c213
YUGESHWARAN G <yugeshwarangm@gmail.com>
2026-02-01 18:50:18 +0530

    Update CORS middleware to allow new origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,8 +48,8 @@ app.include_router(translate.router)
 # )
 app.add_middleware(
     CORSMiddleware,
-    # allow_origin_regex=[ "https://voicenotes-alpha.vercel.app/", r"http://(localhost|127\.0\.0\.1):\d+"],
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_origin_regex=[ "https://speech-recognition-notes-python-qvy.vercel.app", r"http://(localhost|127\.0\.0\.1):\d+"],
+    # allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
